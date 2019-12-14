bc4ade7d7b347d5b0ea53cb9bb822ec0f3e649ce
wydryd125 <ljj4904@gmail.com>
2019-12-14 18:23:36 +0900

    폴더추가

---
+++ swift/191102/20191102_Loop.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var i = 1
+while i<11{
+    print(i)
+    i += 1
+}
+    
---
+++ swift/191102/20191102_var-let.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+
+let maxNumber = 280
+var currentNumber = 123
+currentNumber = 230
+maxNumber = 350
+
+//let으로 maxNumber라는 개체를 생성하였고 저 개체의 값은 300이며, 코드를 수정하지 않는 한 변경할수 없다.
+
