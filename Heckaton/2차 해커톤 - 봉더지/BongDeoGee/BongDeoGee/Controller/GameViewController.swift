84b2269c1c3b82f3492f336943f0fb31ca0f75c8
wydryd125 <ljj4904@gmail.com>
2020-02-11 21:17:04 +0900

    봉더지 파일 수정
    지승코드 및 전체 코드 해석, 주석달기

--- Heckaton/2차 해커톤 - 봉더지/BongDeoGee/BongDeoGee/Controller/GameViewController.swift
+++ Heckaton/2차 해커톤 - 봉더지/BongDeoGee/BongDeoGee/Controller/GameViewController.swift
@@ -8,15 +8,15 @@
 import AudioToolbox.AudioServices
 import AVFoundation
 import UIKit
-
+//!!!!!!!!!!!!!!!!
 var levelIdx = 0
 
 class GameViewController: UIViewController {
-    
+
     var itemCount = 3
     var gameLevel: Double
     
-    
+    // 보너스업쓰가 랜덤 위치로 나오게 하기 위해 배열에 위치 여러곳을 넣어둠.
     private let randomBounsFrame = [
         CGRect(x: 70, y: 80, width: 110, height: 110),
         CGRect(x: 60, y: 200, width: 100, height: 100),
@@ -27,25 +27,26 @@ class GameViewController: UIViewController {
     ]
     
     var tempIndexPath: Int?
-    
     var bbongsStatus = false
-    
     var level = 1
+    
+    // 점수는 계속 입력받아야하므로 willset으로 값 받기... 그리고 그 값(newValue)은 currentScoreView.scoreText에 쏙!
     var score: Int = 0 {
         willSet {
             currentScoreView.scoreText = newValue
         }
     }
-    
+    //타이머들
     private var gameTimer = Timer()
     private var itemTimer = Timer()
     
+    //시간도 계속 바뀌므로 willset으로... 그리고 그 값(newValue)은 string으로 바꿔서 currentScoreView.timerText 쏙!
     var counter: Int = 0 {
         willSet {
             currentScoreView.timerText = TimeToString(counter: newValue)
         }
     }
-    
+    //!!!!!!!!!!!!!!!!!!!
     var sec = 0 {
         willSet {
             gameOver(time: newValue)
@@ -54,8 +55,6 @@ class GameViewController: UIViewController {
     var nano = 0
     
     private var isState = true
-
-    
     private var soundEffect = AVAudioPlayer()
     private let backgroundImage = UIImageView()
     private let currentScoreView = CurrentScoreView()
@@ -66,11 +65,12 @@ class GameViewController: UIViewController {
     
     private let layout = UICollectionViewFlowLayout()
     lazy var collectionView = UICollectionView(frame: view.frame, collectionViewLayout: layout)
-    
+    //
     init(level: GameSet) {
         gameLevel = level.interval
         print(gameLevel)
         super.init(nibName: nil, bundle: nil)
+
     }
     
     required init?(coder: NSCoder) {
@@ -82,25 +82,29 @@ class GameViewController: UIViewController {
         collectionViewUI()
         autolayout()
         backgroundViewUI()
+        //여러가지 셀선택이 가능함 우리 봉더지에는 필요없음. 기본값이 false여서 사용하지 않으면 굳이 안써줘두댐ㅎ
         collectionView.allowsMultipleSelection = false
         controlAction()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+       // 시간 및 이미지, 점수 초기화를 위해 viewWillAppear에 ^^!!
         reloadData()
     }
     
     private func reloadData() {
+        // timetostring을 써주었기때문에 0만 써놔두댐
         counter = 00
-        sec = 00
-        nano = 00
-        score = 00
+        sec = 0
+        nano = 0
+        score = 0
         controlView.startButton.setImage(UIImage(named: "시작"), for: .normal)
         currentScoreView.timerText = "READY!"
+        //reloadData - 데이터를 초기화하는 함수, 기본적으로 있음.
         collectionView.reloadData()
     }
-    
+    //비지엠 나오게하는 함수~ 그냥 똑같이쓰세염
     private func BGMPlayer() {
         let url = Bundle.main.url(forResource: "큰일났어요", withExtension: "mp3")
         if let url = url {
@@ -115,8 +119,9 @@ class GameViewController: UIViewController {
         }
     }
 
-    
+    // 게임오버하느 함수
     private func gameOver(time: Int) {
+        // 타이머가 15초가 되면~~~~~~~~~~ 게임레벨이랑 스코어 들고 가버릴거야~~~~~~~~~~~~
         if time == 15 {
             let gameOverVC = GameOverViewController(level: gameLevel, score: score)
             gameOverVC.modalPresentationStyle = .fullScreen
@@ -124,15 +129,15 @@ class GameViewController: UIViewController {
             stopTimer()
         }
     }
-    
+   // 플레이버트 눌렀을떄~~
     private func controlAction() {
         controlView.startButton.addTarget(self, action: #selector(didControlAction(_:)), for: .touchUpInside)
     }
-    
+  // 플레이 버튼 누르면 일어나는 일~~~~~~~~~~~₩
     @objc func didControlAction(_ button: UIButton) {
         switch button {
         case controlView.startButton:
-            if isState {
+            if isState /*true*/{
                 BGMPlayer()
                 startTimer()
                 controlView.startButton.setImage(UIImage(named: "정지"), for: .normal)
@@ -148,7 +153,7 @@ class GameViewController: UIViewController {
             break
         }
     }
-    
+    //타이머 멈추는...
     private func stopTimer() {
         gameTimer.invalidate()
         itemTimer.invalidate()
@@ -158,16 +163,15 @@ class GameViewController: UIViewController {
         score = 0
         currentScoreView.timerText = "READY!"
     }
-    
+    //타치머 시작하는..
     func startTimer() {
         gameTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timeAction), userInfo: nil, repeats: true)
         itemTimer = Timer.scheduledTimer(timeInterval: gameLevel, target: self, selector: #selector(itemAction), userInfo: nil, repeats: true)
     }
-    
     @objc func timeAction() {
         counter += 1
     }
-    
+    // 시간을 문자열로~
     private func TimeToString (counter: Int) -> String {
         sec = (counter / 100) % 60
         nano = counter % 100
@@ -181,56 +185,71 @@ class GameViewController: UIViewController {
             randomPopUp()
         }
         
+        //보너스업쓰를 랜덤한 시간으로 띄우기 위함...
         let temp = [1, 2, 5, 7]
         if sec % 3 == temp.randomElement() {
-//            print(sec)
             bonusPopUp()
         } else {
             bonusButton.isHidden = true
         }
     }
-    
+    //[0,1,2,3,4,5,6,7,8] 9게
     private func makeItemCount() -> [Int] {
         var array: [Int] = []
-        for i in 0...(itemCount*itemCount-1) {
+                      //3*3-1 = 8
+        for i in 0..<itemCount*itemCount {
             array.append(i)
         }
-        return array
+        return array //[0,1,2,3,4,5,6,7,8]
     }
-    
+    //보너스 업쓰를 띄우는...
     private func bonusPopUp() {
+        //랜덤보너스프레임중 랜덤한 위치를 tempRandomFrame 요기에 넣어줌
         guard let tempRandomFrame = randomBounsFrame.randomElement() else { return }
+        //이제 보너스업스는 랜덤한 위치에 자리한다...!
         bonusButton.frame = tempRandomFrame
         bonusButton.isHidden = false
     }
-    
+    //봉쓰를 띄우는....
     private func bbongsPopUp() {
+        //[0,1,2,3,4,5,6,7,8]중 랜덤값을 intIndex에 넣어둠.
         guard let intIndex = makeItemCount().randomElement() else { return }
+       //[0,1,2,3,4,5,6,7,8]중 랜덤값이 [0,1,2,3,4,5,6,7,8]중 랜덤값과 같으면 봉쓰업
         if intIndex == tempIndexPath {
             bbongsPopUp()
         } else {
             let indexPath = randomIndex()
+            //selectItem - > isSectected true로 만들어줌, randomIndex값이 indexPath값이 되어 그 인덱스만 true가됨ㄴ...
+            //뭔말이야 씨아런미로암너ㅏㅇ뢈노란올ㄴㅇ
             collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredVertically)
+            // 봉쓰이미지를 나오게~~
             bbongsStatus = true
         }
     }
-    
+    //랜덤으로 띄우는 함수
     private func randomPopUp() {
+        //indexPath는 랜덤한 숫자를 뽑는 함수에서 나온 값~~~~~~~~~
         let indexPath = randomIndex()
         collectionView.selectItem(at: indexPath, animated: true, scrollPosition: .centeredVertically)
+        // 두더지 이미지 나오게~~~
         bbongsStatus = false
     }
-    
+    //랜덤한 숫자를 뽑기위한 함수
     private func randomIndex() -> IndexPath {
+        //randomInt = [0,1,2,3,4,5,6,7,8]중 랜덤값
         guard let randomInt = makeItemCount().randomElement() else { return IndexPath(item: 0, section: 0)}
+       // tempIndexPath = [0,1,2,3,4,5,6,7,8]중 랜덤값 과 randomInt가 같으면
         if tempIndexPath == randomInt {
+           //랜덤숫자 리턴
             return randomIndex()
         } else {
+            //같지않으면 tempIndexPath값은 randomInt값을...
+            //값을 여기에 넣어줘서 중복방지를 해줌.
             tempIndexPath = randomInt
             return IndexPath(item: randomInt, section: 0)
         }
     }
-    
+    //보너스 업스를 누르면~~~~~~~~~~~
     @objc func bonusButtonDidTap() {
         let bonusOrigin = bonusWordImage.transform
         
@@ -330,9 +349,10 @@ class GameViewController: UIViewController {
         bonusWordImage.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
 }
-
+//델리게이트사용~~~~~~~~~~~~~~~~~~~~~~~>_<
 extension GameViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //makeItemCount() -> [0,1,2,3,4,5,6,7,8]
         return makeItemCount().count
     }
     
@@ -343,69 +363,76 @@ extension GameViewController: UICollectionViewDataSource {
     }
     
 }
-
+//CustomCollectionCell에서 만든 함수 가져와서 사용
 extension GameViewController: CustomCollectionCellDelegate {
-    
+    //버튼이 눌렸을때 일어나는
     func actionButton(image: UIImageView) {
+        //버튼에 있는 이미지가~ 봉쓰와 두더지없음 이면 vibrate() 함수실행
         switch image.image {
         case UIImage(named: "두더지"):
-//            print("dodo")
             break
         case UIImage(named: "봉쓰"):
             vibrate()
-//            print("Fire")
+
         case UIImage(named: "보너스"):
-//            print("Bonus")
+
             break
         case UIImage(named: "두더지없음"):
             vibrate()
+            //게임타이너가 유효하면(ture) 밑에처럼 실행하겠다. 아니면 실행할거 없음.
             if gameTimer.isValid == true {
                 misBackgroundView.alpha = 1
                 UIView.animate(withDuration: 0.3) {
                     self.misBackgroundView.alpha = 0
                 }
             }
-//            print("땡")
         default:
             break
         }
     }
-    
+    // 진동을 주기위한 함수, 맨 위 상단 import AudioToolbox.AudioServices 넣어줘야함.
     private func vibrate() {
         AudioServicesPlaySystemSound(kSystemSoundID_Vibrate)
     }
-    
+    // 셀 변경을 위한 액션
     func cellChangeAction(isSelected: Bool, cell: CustomCollectionCell) {
+        //isSelected true
         if isSelected {
+            //  bbongsStatus true
             if bbongsStatus {
                 cell.imageView.image = UIImage(named: "봉쓰")
                 cell.imageView.contentMode = .scaleAspectFit
+            //  bbongsStatus false
             } else   {
                 cell.imageView.image = UIImage(named: "두더지")
                 cell.imageView.contentMode = .scaleAspectFill
             }
+            //isSelected false
         } else {
             cell.imageView.image = UIImage(named: "두더지없음")
             cell.imageView.contentMode = .scaleAspectFill
         }
         
     }
+    //타이머 재실행 함수
     func reStartTimer() {
         gameTimer.fire()
         itemTimer.fire()
     }
-    
+    //커스텀셀 눌렀을때
     func buttonAction(cell: CustomCollectionCell) {
-        guard
+        guard // 내가 눌렀을때 셀의 인덱스패스를 가져오기 위함.
             let tapIndexPath = collectionView.indexPath(for: cell),
+            // 두더지나, 봉쓰처럼 현재 나오는 것들 중 첫번째 인덱스패스를 가져옴
             let selectIndexPath = collectionView.indexPathsForSelectedItems?.first
             else { return }
         
+        //내가 누른것과, 두더지 or 봉쓰가 같았을때
         if tapIndexPath == selectIndexPath {
             switch cell.imageView.image {
             case UIImage(named: "두더지"):
                 score += 100
-                
+                //웁스이미지 애니메이션
                 let dodeogeeOrigin = cell.oopsImage.transform
                 cell.oopsImage.alpha = 1
                 cell.oopsImage.transform = cell.oopsImage.transform.scaledBy(x: 1.5, y: 1.5)
@@ -418,11 +445,14 @@ extension GameViewController: CustomCollectionCellDelegate {
                 })
             case UIImage(named: "봉쓰"):
                 score = 0
+                //타이머 스도오오옵
                 gameTimer.invalidate()
                 itemTimer.invalidate()
+                //봉쓰 확대뷰 나오게... 다시 게임뷰로 돌아오는건 봉쓰확대뷰에서 해야함.
                 let fireVC = FirebongViewController()
                 fireVC.modalPresentationStyle = .overCurrentContext
                 present(fireVC, animated: false)
+                //다시 타이머 시작
                 reStartTimer()
             default:
                 break
--- Heckaton/2차 해커톤 - 봉더지/BongDeoGee/BongDeoGee/View/CustomCollectionCell.swift
+++ Heckaton/2차 해커톤 - 봉더지/BongDeoGee/BongDeoGee/View/CustomCollectionCell.swift
@@ -7,72 +7,76 @@
 //
 
 import UIKit
-
+//GameViewController에서 사용할 함수들을 프로토콜로 모아둠
 protocol CustomCollectionCellDelegate: class {
-  func buttonAction(cell: CustomCollectionCell)
-  func actionButton(image: UIImageView)
-  func cellChangeAction(isSelected: Bool, cell: CustomCollectionCell)
+    func buttonAction(cell: CustomCollectionCell)
+    func actionButton(image: UIImageView)
+    func cellChangeAction(isSelected: Bool, cell: CustomCollectionCell)
 }
 
 class CustomCollectionCell: UICollectionViewCell {
-  
-  static let identifier = "cell"
-  
-  weak var delegate: CustomCollectionCellDelegate?
- 
-  let oopsImage = UIImageView()
-  let imageView = UIImageView()
-  private let selectButton = UIButton()
-  
-  override var isSelected: Bool {
-    didSet {
-      delegate?.cellChangeAction(isSelected: self.isSelected, cell: self)
+    //identifier를 실수하지 않게..
+    static let identifier = "cell"
+    //델리게이트 사용시 필수 작성~.~
+    weak var delegate: CustomCollectionCellDelegate?
+    
+    let oopsImage = UIImageView()
+    let imageView = UIImageView()
+    private let selectButton = UIButton()
+    
+    //이미 만들어져있는 isSelected를 가져와서 사용함
+    //Bool값이 자꾸 변하니까 델리게이트로 그값을 알려줌.
+    override var isSelected: Bool {
+        didSet {
+            delegate?.cellChangeAction(isSelected: self.isSelected, cell: self)
+        }
     }
-  }
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    baseUI()
-    selectButton.addTarget(self, action: #selector(didTabButton), for: .touchUpInside)
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  @objc func didTabButton() {
-    delegate?.buttonAction(cell: self)
-    delegate?.actionButton(image: imageView)
-  }
-  
-  private func baseUI() {
-    imageView.image = UIImage(named: "두더지없음")
-    imageView.contentMode = .scaleAspectFill
     
-    contentView.addSubview(imageView)
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-    imageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-    imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-    imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        baseUI()
+
+    }
     
-    oopsImage.image = UIImage(named: "웁쓰")
-    oopsImage.contentMode = .scaleAspectFill
-    oopsImage.alpha = 0
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //버튼이 눌렀을때의 일을 델리게이트에게 위임?
+    @objc func didTabButton() {
+        delegate?.buttonAction(cell: self)
+        delegate?.actionButton(image: imageView)
+    }
     
-    contentView.addSubview(oopsImage)
-    oopsImage.translatesAutoresizingMaskIntoConstraints = false
-    oopsImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4).isActive = true
-    oopsImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-    oopsImage.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.5).isActive = true
-    oopsImage.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.3).isActive = true
+    private func baseUI() {
+        imageView.image = UIImage(named: "두더지없음")
+        imageView.contentMode = .scaleAspectFill
+        
+        selectButton.addTarget(self, action: #selector(didTabButton), for: .touchUpInside)
+        
+        contentView.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        oopsImage.image = UIImage(named: "웁쓰")
+        oopsImage.contentMode = .scaleAspectFill
+        oopsImage.alpha = 0
+        
+        contentView.addSubview(oopsImage)
+        oopsImage.translatesAutoresizingMaskIntoConstraints = false
+        oopsImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 4).isActive = true
+        oopsImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        oopsImage.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.5).isActive = true
+        oopsImage.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.3).isActive = true
+        
+        contentView.addSubview(selectButton)
+        selectButton.translatesAutoresizingMaskIntoConstraints = false
+        selectButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        selectButton.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        selectButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        selectButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+    }
     
-    contentView.addSubview(selectButton)
-    selectButton.translatesAutoresizingMaskIntoConstraints = false
-    selectButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
-    selectButton.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
-    selectButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
-    selectButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-  }
-  
 }
